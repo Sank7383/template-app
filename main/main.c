eba0bb42406e65030b9fdc0726e9d8150079668a
Sandeep <sandeepkushwaha.netsol@gmail.com>
2025-11-13 11:50:16 +0530

    inital commit

---
+++ main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+
+// Define GPIO pin for LED
+#define LED_GPIO_PIN GPIO_NUM_2
+
+void app_main(void)
+{
+
+}
