d8483fea9b457082c6cc02a35e1d11fefe1b15eb
Sandeep <sandeepkushwaha.netsol@gmail.com>
2025-11-13 13:49:15 +0530

    LED blinking code

--- main/main.c
+++ main/main.c
@@ -3,10 +3,28 @@
 #include <freertos/task.h>
 #include <driver/gpio.h>
 
-// Define GPIO pin for LED
-#define LED_GPIO_PIN GPIO_NUM_2
+// Define the GPIO pin to blink
+#define BLINK_GPIO 12
 
 void app_main(void)
 {
+// Reset the pin to a known state
+    gpio_reset_pin(BLINK_GPIO);
+    
+    // Set the GPIO pin as a push/pull output
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+// Infinite loop
+    while(1) {
+        /* Set GPIO level low (turn LED off) */
+        gpio_set_level(BLINK_GPIO, 0);
+        
+        // Wait for 1000 milliseconds (1 second)
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
 
+        /* Set GPIO level high (turn LED on) */
+        gpio_set_level(BLINK_GPIO, 1);
+
+        // Wait for 1000 milliseconds (1 second)
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
