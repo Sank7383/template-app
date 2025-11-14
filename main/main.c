cd54f52e1907444f359b10a529b0afa804f54951
Sandeep <sandeepkushwaha.netsol@gmail.com>
2025-11-14 16:19:59 +0530

    i2c scanner code

--- main/main.c
+++ main/main.c
@@ -1,30 +1,52 @@
 #include <stdio.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <driver/gpio.h>
+#include "driver/i2c_master.h"
+#include "esp_log.h"
 
-// Define the GPIO pin to blink
-#define BLINK_GPIO 12
+#define I2C_MASTER_SCL_IO           22
+#define I2C_MASTER_SDA_IO           19
+#define I2C_MASTER_PORT             I2C_NUM_0
+#define I2C_MASTER_FREQ_HZ          100000
+
+static const char *TAG = "I2C_SCAN";
 
 void app_main(void)
 {
-// Reset the pin to a known state
-    gpio_reset_pin(BLINK_GPIO);
-    
-    // Set the GPIO pin as a push/pull output
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-// Infinite loop
-    while(1) {
-        /* Set GPIO level low (turn LED off) */
-        gpio_set_level(BLINK_GPIO, 0);
-        
-        // Wait for 1000 milliseconds (1 second)
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    ESP_LOGI(TAG, "Starting I2C Scanner...");
 
-        /* Set GPIO level high (turn LED on) */
-        gpio_set_level(BLINK_GPIO, 1);
+    // 1. Create config
+    i2c_master_bus_config_t bus_config = {
+        .i2c_port = I2C_MASTER_PORT,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+    };
 
-        // Wait for 1000 milliseconds (1 second)
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    i2c_master_bus_handle_t bus_handle;
+
+    // 2. Create I2C bus
+    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &bus_handle));
+
+    // 3. Scan all addresses
+    for (int addr = 1; addr < 127; addr++) {
+        i2c_master_dev_handle_t dev_handle;
+        i2c_device_config_t dev_cfg = {
+            .device_address = addr,
+            .scl_speed_hz = I2C_MASTER_FREQ_HZ,
+        };
+
+        esp_err_t add_result = i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle);
+
+        if (add_result == ESP_OK) {
+            esp_err_t ret = i2c_master_probe(bus_handle, addr, 1000);
+
+            if (ret == ESP_OK) {
+                ESP_LOGI(TAG, "I2C device found at 0x%02X", addr);
+            }
+
+            i2c_master_bus_rm_device(dev_handle);
+        }
     }
+
+    ESP_LOGI(TAG, "Scan Completed.");
 }
